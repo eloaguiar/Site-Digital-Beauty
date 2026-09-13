6a72066c0f583b5323fe96c96d08be95d97e7aad
eloaguiar <eloaguiar04@gmail.com>
2026-09-13 17:06:04 -0300

    Add files via upload

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,6 +13,19 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
+const metaPixelScript = `
+!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '364252861208280');
+fbq('track', 'PageView');
+`;
+
 export const metadata: Metadata = {
   metadataBase: new URL("https://digital-beauty-dsn.brunopollnow.chatgpt.site"),
   title: "Digital Beauty",
@@ -41,9 +54,21 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-BR">
+      <head>
+        <script dangerouslySetInnerHTML={{ __html: metaPixelScript }} />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <noscript>
+          <img
+            height={1}
+            width={1}
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=364252861208280&ev=PageView&noscript=1"
+            alt=""
+          />
+        </noscript>
         {children}
       </body>
     </html>
